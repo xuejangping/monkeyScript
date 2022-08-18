c681ba9efe4034c9ccdd094d9afdebeb366cfbe1
112752 <1127523185@qq.com>
2022-08-18 23:24:57 +0800

    1

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  个人测试脚本!
 // @author       You
-// @match        http://*www.baidu.com/*
+// @match        http*://*www.baidu.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @license MIT
